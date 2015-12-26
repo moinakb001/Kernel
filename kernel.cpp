5b1aebf359d2b77ce0c3cfa7a59e2c3a7e01704e
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2015-12-25 23:53:09 -0800

    test

--- kernel.cpp
+++ kernel.cpp
@@ -82,6 +82,9 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
 }
  
 void terminal_putchar(char c) {
+	if(c=='\n'){
+		terminal_row++;
+	}
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
@@ -108,5 +111,5 @@ void kernel_main() {
          * yet, '\n' will produce some VGA specific character instead.
          * This is normal.
          */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello, kernel World!\nTEST");
 }
