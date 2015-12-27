17e42bc3eff7ff791358d1e883a7dc9c5e4bf3c1
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2015-12-26 23:18:09 -0800

    fix 2

--- string.cpp
+++ string.cpp
@@ -1,9 +1,9 @@
 #include "string.h"
 namespace{
-	uint8_t * message="FUCK OFF NOT IMPLEMENTED";
+	 uint8_t * message="FUCK OFF NOT IMPLEMENTED";
 }
 namespace std{
-	void * memcpy( void  * destination, const void * source, uint32_t num ){
+	void * memcpy( void  * destination,  void * source, uint32_t num ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		for(uint32_t i=0;i<num;i++){
@@ -12,7 +12,7 @@ namespace std{
 		return destination;
 	}
 	
-	void * memmove ( void * destination, const void * source, uint32_t num ){
+	void * memmove ( void * destination,  void * source, uint32_t num ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		uint8_t buffer[num];
@@ -25,7 +25,7 @@ namespace std{
 		return destination;
 	}
 	
-	uint8_t * strcpy ( uint8_t * destination, const uint8_t * source ){
+	uint8_t * strcpy ( uint8_t * destination,  uint8_t * source ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		for(uint32_t i=0;srcArr[i]!=0;i++){
@@ -34,7 +34,7 @@ namespace std{
 		return destination;
 	}
 	
-	uint8_t * strncpy ( uint8_t * destination, const uint8_t * source, uint32_t num ){
+	uint8_t * strncpy ( uint8_t * destination,  uint8_t * source, uint32_t num ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		for(uint32_t i=0;srcArr[i]!=0&&i<num;i++){
@@ -45,7 +45,7 @@ namespace std{
 
 	
 	
-	uint8_t * strcat ( uint8_t * destination, const uint8_t * source ){
+	uint8_t * strcat ( uint8_t * destination,  uint8_t * source ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		uint32_t len=strlen(destination);
@@ -55,7 +55,7 @@ namespace std{
 		return destination;
 	}
 	
-	uint8_t * strncat ( uint8_t * destination, const uint8_t * source, uint32_t num ){
+	uint8_t * strncat ( uint8_t * destination,  uint8_t * source, uint32_t num ){
 		uint8_t * destArr=(uint8_t * )destination;
 		uint8_t * srcArr=(uint8_t * )source;
 		uint32_t len=strlen(destination);
@@ -67,7 +67,7 @@ namespace std{
 
 	
 	
-	int32_t memcmp ( const void * ptr1, const void * ptr2, uint32_t num ){
+	int32_t memcmp (  void * ptr1,  void * ptr2, uint32_t num ){
 		uint32_t i=0;
 		uint8_t * ptr1Arr=(uint8_t *)ptr1;
 		uint8_t * ptr2Arr=(uint8_t *)ptr2;
@@ -77,7 +77,7 @@ namespace std{
 		return (int32_t)(ptr1Arr[i]-ptr2Arr[i]);
 		
 	}
-	int32_t strcmp ( const uint8_t * str1, const uint8_t * str2 ){
+	int32_t strcmp (  uint8_t * str1,  uint8_t * str2 ){
 		uint32_t i=0;
 		uint8_t * ptr1Arr=str1;
 		uint8_t * ptr2Arr=str1;
@@ -86,10 +86,10 @@ namespace std{
 		}
 		return (int32_t)(ptr1Arr[i]-ptr2Arr[i]);
 	}
-	int32_t strcoll ( const uint8_t * str1, const uint8_t * str2 ){
+	int32_t strcoll (  uint8_t * str1,  uint8_t * str2 ){
 		return strcmp(str1,str2);
 	}
-	int32_t strncmp ( const uint8_t * str1, const uint8_t * str2, uint32_t num ){
+	int32_t strncmp (  uint8_t * str1,  uint8_t * str2, uint32_t num ){
 		uint32_t i=0;
 		uint8_t * ptr1Arr=str1;
 		uint8_t * ptr2Arr=str1;
@@ -98,7 +98,7 @@ namespace std{
 		}
 		return (int32_t)(ptr1Arr[i]-ptr2Arr[i]);
 	}
-	uint32_t strxfrm ( uint8_t * destination, const uint8_t * source, uint32_t num ){
+	uint32_t strxfrm ( uint8_t * destination,  uint8_t * source, uint32_t num ){
 		strncpy(destination,source, num);
 		return strlen(destination);
 	}
@@ -111,22 +111,22 @@ namespace std{
 	uint8_t * strchr ( uint8_t * str, int32_t character ){
 		
 	}
-	uint32_t strcspn ( const uint8_t * str1, const uint8_t * str2 ){
+	uint32_t strcspn (  uint8_t * str1,  uint8_t * str2 ){
 		
 	}
-	uint8_t * strpbrk ( uint8_t * str1, const uint8_t * str2 ){
+	uint8_t * strpbrk ( uint8_t * str1,  uint8_t * str2 ){
 		
 	}
 	uint8_t * strrchr ( uint8_t * str, int32_t character ){
 		
 	}
-	uint32_t strspn ( const uint8_t * str1, const uint8_t * str2 ){
+	uint32_t strspn (  uint8_t * str1,  uint8_t * str2 ){
 		
 	}
-	uint8_t * strstr ( uint8_t * str1, const uint8_t * str2 ){
+	uint8_t * strstr ( uint8_t * str1,  uint8_t * str2 ){
 		
 	}
-	uint8_t * strtok ( uint8_t * str, const uint8_t * delimiters ){
+	uint8_t * strtok ( uint8_t * str,  uint8_t * delimiters ){
 		
 	}
 
@@ -143,7 +143,7 @@ namespace std{
 	uint8_t * strerror ( int32_t errnum ){
 		return message;
 	}
-	uint32_t strlen ( const uint8_t * str ){
+	uint32_t strlen (  uint8_t * str ){
 		uint32_t i=0;
 		while(str[i]!=0){
 			i++;
--- string.h
+++ string.h
@@ -2,31 +2,31 @@
 #define STRING_H
 #include <stdint.h>
 namespace std{
-void * memcpy( void  * destination, const void * source, uint32_t num );
-void * memmove ( void * destination, const void * source, uint32_t num );
-uint8_t * strcpy ( uint8_t * destination, const uint8_t * source );
-uint8_t * strncpy ( uint8_t * destination, const uint8_t * source, uint32_t num );
+void * memcpy( void  * destination,  void * source, uint32_t num );
+void * memmove ( void * destination,  void * source, uint32_t num );
+uint8_t * strcpy ( uint8_t * destination,  uint8_t * source );
+uint8_t * strncpy ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-uint8_t * strcat ( uint8_t * destination, const uint8_t * source );
-uint8_t * strncat ( uint8_t * destination, const uint8_t * source, uint32_t num );
+uint8_t * strcat ( uint8_t * destination,  uint8_t * source );
+uint8_t * strncat ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-int32_t memcmp ( const void * ptr1, const void * ptr2, uint32_t num );
-int32_t strcmp ( const uint8_t * str1, const uint8_t * str2 );
-int32_t strcoll ( const uint8_t * str1, const uint8_t * str2 );
-int32_t strncmp ( const uint8_t * str1, const uint8_t * str2, uint32_t num );
-uint32_t strxfrm ( uint8_t * destination, const uint8_t * source, uint32_t num );
+int32_t memcmp (  void * ptr1,  void * ptr2, uint32_t num );
+int32_t strcmp (  uint8_t * str1,  uint8_t * str2 );
+int32_t strcoll (  uint8_t * str1,  uint8_t * str2 );
+int32_t strncmp (  uint8_t * str1,  uint8_t * str2, uint32_t num );
+uint32_t strxfrm ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
 void * memchr ( void * ptr, int32_t value, uint32_t num );
 uint8_t * strchr ( uint8_t * str, int32_t character );
-uint32_t strcspn ( const uint8_t * str1, const uint8_t * str2 );
-uint8_t * strpbrk ( uint8_t * str1, const uint8_t * str2 );
+uint32_t strcspn (  uint8_t * str1,  uint8_t * str2 );
+uint8_t * strpbrk ( uint8_t * str1,  uint8_t * str2 );
 uint8_t * strrchr ( uint8_t * str, int32_t character );
-uint32_t strspn ( const uint8_t * str1, const uint8_t * str2 );
-uint8_t * strstr ( uint8_t * str1, const uint8_t * str2 );
-uint8_t * strtok ( uint8_t * str, const uint8_t * delimiters );
+uint32_t strspn (  uint8_t * str1,  uint8_t * str2 );
+uint8_t * strstr ( uint8_t * str1,  uint8_t * str2 );
+uint8_t * strtok ( uint8_t * str,  uint8_t * delimiters );
 
 void * memset ( void * ptr, int32_t value, uint32_t num );
 uint8_t * strerror ( int32_t errnum );
-uint32_t strlen ( const uint8_t * str );
+uint32_t strlen (  uint8_t * str );
 }
 #endif
