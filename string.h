9d0d573ad68e1dcdd0d42c1faef2494b61a1a621
Moinak Bhattacharyya <mb.totaldomination@gmail.com>
2015-12-27 20:36:38 -0800

    primup

--- string.h
+++ string.h
@@ -1,32 +1,32 @@
 #ifndef STRING_H
 #define STRING_H
 #include <stdint.h>
-namespace std{
-void * memcpy( void  * destination,  void * source, uint32_t num );
-void * memmove ( void * destination,  void * source, uint32_t num );
-uint8_t * strcpy ( uint8_t * destination,  uint8_t * source );
-uint8_t * strncpy ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-uint8_t * strcat ( uint8_t * destination,  uint8_t * source );
-uint8_t * strncat ( uint8_t * destination,  uint8_t * source, uint32_t num );
+extern "C" void * memcpy( void  * destination,  void * source, uint32_t num );
+extern "C" void * memmove ( void * destination,  void * source, uint32_t num );
+extern "C" uint8_t * strcpy ( uint8_t * destination,  uint8_t * source );
+extern "C" uint8_t * strncpy ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-int32_t memcmp (  void * ptr1,  void * ptr2, uint32_t num );
-int32_t strcmp (  uint8_t * str1,  uint8_t * str2 );
-int32_t strcoll (  uint8_t * str1,  uint8_t * str2 );
-int32_t strncmp (  uint8_t * str1,  uint8_t * str2, uint32_t num );
-uint32_t strxfrm ( uint8_t * destination,  uint8_t * source, uint32_t num );
+extern "C" uint8_t * strcat ( uint8_t * destination,  uint8_t * source );
+extern "C" uint8_t * strncat ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-void * memchr ( void * ptr, int32_t value, uint32_t num );
-uint8_t * strchr ( uint8_t * str, int32_t character );
-uint32_t strcspn (  uint8_t * str1,  uint8_t * str2 );
-uint8_t * strpbrk ( uint8_t * str1,  uint8_t * str2 );
-uint8_t * strrchr ( uint8_t * str, int32_t character );
-uint32_t strspn (  uint8_t * str1,  uint8_t * str2 );
-uint8_t * strstr ( uint8_t * str1,  uint8_t * str2 );
-uint8_t * strtok ( uint8_t * str,  uint8_t * delimiters );
+extern "C" int32_t memcmp (  void * ptr1,  void * ptr2, uint32_t num );
+extern "C" int32_t strcmp (  uint8_t * str1,  uint8_t * str2 );
+extern "C" int32_t strcoll (  uint8_t * str1,  uint8_t * str2 );
+extern "C" int32_t strncmp (  uint8_t * str1,  uint8_t * str2, uint32_t num );
+extern "C" uint32_t strxfrm ( uint8_t * destination,  uint8_t * source, uint32_t num );
 
-void * memset ( void * ptr, int32_t value, uint32_t num );
-uint8_t * strerror ( int32_t errnum );
-uint32_t strlen (  uint8_t * str );
-}
+extern "C" void * memchr ( void * ptr, int32_t value, uint32_t num );
+extern "C" uint8_t * strchr ( uint8_t * str, int32_t character );
+extern "C" uint32_t strcspn (  uint8_t * str1,  uint8_t * str2 );
+extern "C" uint8_t * strpbrk ( uint8_t * str1,  uint8_t * str2 );
+extern "C" uint8_t * strrchr ( uint8_t * str, int32_t character );
+extern "C" uint32_t strspn (  uint8_t * str1,  uint8_t * str2 );
+extern "C" uint8_t * strstr ( uint8_t * str1,  uint8_t * str2 );
+extern "C" uint8_t * strtok ( uint8_t * str,  uint8_t * delimiters );
+
+extern "C" void * memset ( void * ptr, int32_t value, uint32_t num );
+extern "C" uint8_t * strerror ( int32_t errnum );
+extern "C" uint32_t strlen (  uint8_t * str );
+
 #endif
